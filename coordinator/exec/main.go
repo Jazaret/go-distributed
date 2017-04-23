63a3d31d154fb4e0beaa147e69cf88dec43e0404
Javier Azaret <jazaret@gmail.com>
2017-04-23 13:39:35 -0400

    bug fix

--- coordinator/exec/main.go
+++ coordinator/exec/main.go
@@ -10,7 +10,7 @@ var dc *coordinator.DatabaseConsumer
 
 func main() {
 	ea := coordinator.NewEventAggregator()
-	dc := coordinator.NewDatabaseConsumer(ea)
+	dc = coordinator.NewDatabaseConsumer(ea)
 	ql := coordinator.NewQueueListener(ea)
 	go ql.ListenForNewSource()
 
